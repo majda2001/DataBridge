16d63a11928896dc9270b1258a531974b076d886
majda2001 <ablaoui.majda2001@gmail.com>
2026-02-19 11:58:36 +0000

    add vercel config

--- DataBridge/wsgi.py
+++ DataBridge/wsgi.py
@@ -9,4 +9,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DataBridge.settings')
 
-app = get_wsgi_application()  
+application = get_wsgi_application()  
+app = application
